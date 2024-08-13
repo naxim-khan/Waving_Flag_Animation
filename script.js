3c105aecd28333afe0893d3129590da28f814250
naxim-khan <nazeemkhanpk@gmail.com>
2024-08-13 20:19:40 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const flagEl = document.getElementById("flag");
+
+const rows = 50;
+const columns = 75;
+
+const centerColumn = Math.floor(columns / 2);
+const centerRow = Math.floor(rows / 2);
+
+const outerRadius = 12; 
+const innerRadius = 9;  
+
+const starSize = 3;
+
+const generateUnit = (columnNum, rowNum) => {
+  const flagUnit = document.createElement("div");
+  flagUnit.classList.add("flag-unit");
+  if (columnNum < 15) {
+    flagUnit.style.backgroundColor = "white";
+  }
+  // Distance from center
+  const distanceFromCenter = Math.sqrt(
+    Math.pow(columnNum - centerColumn, 2) + Math.pow(rowNum - centerRow, 2)
+  );
+
+  const isInCrescent = (
+    distanceFromCenter < outerRadius && distanceFromCenter > innerRadius &&
+    columnNum < centerColumn 
+  );
+
+  const isInStar = (
+    Math.abs(columnNum - centerColumn) <= starSize &&
+    Math.abs(rowNum - centerRow) <= starSize &&
+    (Math.abs(columnNum - centerColumn) + Math.abs(rowNum - centerRow)) <= starSize
+  );
+
+  if (isInCrescent || isInStar) {
+    flagUnit.style.backgroundColor = "white";
+  }
+
+  flagUnit.style.setProperty("animation-delay", `${columnNum * 10}ms`);
+  flagUnit.style.setProperty("--displacement", `${columnNum / 4}px`);
+
+  const column = document.getElementById(`column-${columnNum}`);
+  column.innerHTML += flagUnit.outerHTML;
+};
+
+for (let i = 0; i < columns; i++) {
+  flagEl.innerHTML += `<div class="column" id="column-${i}"></div>`;
+  for (let j = 0; j < rows; j++) {
+    generateUnit(i, j);
+  }
+}
